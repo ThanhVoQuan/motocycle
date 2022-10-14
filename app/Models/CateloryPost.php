e634df2840a8ec0e4fd809073e26e106d579244b
ThanhVoQuan <109636614+ThanhVoQuan@users.noreply.github.com>
2022-10-14 11:42:04 +0700

    thanh2-upload2

--- app/Models/CateloryPost.php
+++ app/Models/CateloryPost.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class CateloryPost extends Model
 {
     use HasFactory;
-    public $timestamps = true;
+    public $timestamps = false;
     protected $table='moto';
 }
